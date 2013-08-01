023f88468ce42f1e81ad0f37f0e539486e5554bb
James Jesudason <james.jesudason@gmail.com>
2013-08-01 08:57:04 +0100

    Changes to the initialisation for JQuery and Phonegap. Changed the viewport setting to make text the right size for the device.

--- js/index.js
+++ js/index.js
@@ -19,7 +19,58 @@
  
 var user_credentials;
 var BASE_URL = "http://192.168.1.64:5000/rest/v1.0/";
- 
+
+var jqmReady = $.Deferred();
+var pgReady = $.Deferred();
+
+var app = {
+   //Callback for when the app is ready
+   callback: null,
+   // Application Constructor
+   initialize: function(callback) {
+      this.callback = callback;
+      var browser = document.URL.match(/^https?:/);
+      if(browser) {
+         console.log("Is web.");
+         //In case of web we ignore PG but resolve the Deferred Object to trigger initialization
+	 pgReady.resolve();
+      }
+      else {
+         console.log("Is not web.");
+	 this.bindEvents();
+      }
+   },
+   bindEvents: function() {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+   },
+   onDeviceReady: function() {
+      // The scope of 'this' is the event, hence we need to use app.
+      app.receivedEvent('deviceready');
+   },
+   receivedEvent: function(event) {
+      switch(event) {
+         case 'deviceready':
+	    pgReady.resolve();
+	    break;
+      }
+   }
+};
+$(document).on("pageinit", function(event, ui) {
+   jqmReady.resolve();
+});
+/**
+ * General initialization.
+ */
+$.when(jqmReady, pgReady).then(function() {
+   //Initialization code here
+   if(app.callback) {
+      app.callback();
+   }
+   console.log("Frameworks ready.");
+});
+
+
+/*
 var app = {
     // Application Constructor
     initialize: function() {
@@ -36,6 +87,7 @@ var app = {
     onDeviceReady: function() {
     },
 };
+*/
 
 // Check if the user is authorized, or redirect to login page
 function authorization()
